0f24e1ad0b6d19844f10168635e0a6c45f0f9361
Kaydenccc <ffadilah682@gmail.com>
2023-01-10 01:03:50 +0800

    pushh

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+import env from 'dotenv';
+env.config();
 export const config = {
   user: 'ffadilah682@gmail.com',
   clientId: '119738350431-67sjbtqcs8336u4ppnqiuvd34h4beihi.apps.googleusercontent.com',
-  clientSecret: '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',
+  clientSecret: process.env.G_CLIENT_SECRET,
   refreshToken: '1//04wSQQEXZEQ9ACgYIARAAGAQSNwF-L9IrrFKuCAfzjg0leUb_YwUwB3ijhJFsZU7ekXfdJacgVem1FMK5M99Lq_fpPJCxWBQ86n8',
 };
